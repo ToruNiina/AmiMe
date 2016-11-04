ea6e44fec0c5481a8f00591c39fea9fd2f4b5b1d
ToruNiina <niina.toru.68u@gmail.com>
2016-11-04 17:28:52 +0900

    add solverT as a template parameter of simulator

--- amime/core/Simulator.hpp
+++ amime/core/Simulator.hpp
@@ -5,36 +5,43 @@
 namespace amime
 {
 
-template<typename traitsT>
+template<typename traitsT, template<typename T>class solverT>
 class Simulator
 {
   public:
     typedef traitsT traits_type;
+    typedef solverT<traits_type> solver_type;
     typedef typename traits_type::time_type time_type;
     typedef typename traits_type::state_type state_type;
-    typedef typename traits_type::solver_type solver_type;
 
     typedef Network<traits_type> network_type;
     typedef typename network_type::node_type node_type;
     typedef typename network_type::node_id_type node_id_type;
 
   public:
-    Simulator(): time_(0), end_time_(std::numeric_limits<time_type>::max()){}
+    Simulator(const time_type end_time,
+              const solver_type& solver, const network_type& net)
+        : time_(0), end_time_(end_time), solver_(solver), network_(net)
+    {}
     ~Simulator() = default;
 
-    bool step() {this->step_(); return time_ < end_time_;}
-    void step_until(const time_type interval);
+    bool step(){this->step_(); return (time_ < end_time_);}
+    void step_until(const time_type t){while(this->time < t){this->step_();}}
+
+    network_type const& network() const {return network_;}
+    network_type &      network()       {return network_;}
+    solver_type const& solver() const {return solver_;}
+    solver_type &      solver()       {return solver_;}
 
   private:
-    void step_();
+    void step_(){this->time_ = this->solver_(this->time, network);}
 
   private:
-    time_type   time_;
-    time_type   end_time_;
-    solver_type solver_;
+    time_type    time_;
+    time_type    end_time_;
+    solver_type  solver_;
     network_type network_;
 };
-
 
 } // amime
 #endif /* AMIME_CORE_SIMULATOR */
